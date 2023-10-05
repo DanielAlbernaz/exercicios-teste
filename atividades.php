6aa7c77e4189b6039dc6b312fbd7fa1f3c8476f5
LAPTOP-RVP0I873\danie <danielgomesalbernaz@gmail.com>
2023-10-05 10:10:29 -0300

    commit initial

---
+++ atividades.php
@@ -0,0 +1,201 @@
+<?php
+
+/**  Todos execicios usando ("se necessario") IF, ELSE, FOR, FOREACH, WHILE, SWITCH
+ * Operadores 
+ *  && - E 
+ *  || - OU
+ * 
+ * Tipos primitivos:
+ *  $var = array(); - array(vetor)
+ *  $var = 'Daniel' - string
+ *  $var = 1 - inteiro
+ *  $var = 1.2 - float ou doble
+ *  $var = false - boolean
+ * 
+ * Relacionais:
+ * = - atribuição
+ * == igualdade
+ * === igualdade e tipo primitivo
+ * <> ou != - diferente
+ * >= - Maior ou igual
+ * <= - Menor ou igual
+ * > - maior
+ * < - Menor
+ * 
+*/
+
+
+/**
+ * Exemplo de uma solução de execicio
+ * 
+ * Crie um algoritmo que pergunte ao usuário seu nome e sua idade. Em seguida verifique se a idade é maior ou menor que 18, exiba da seguinte forma: Fulano é maior de 18 e tem XX Anos ou Fulano não é maior de 18 e tem XX Anos.
+ * 
+ */
+echo 'Exercício exemplo: <br>';
+
+$nome = 'Lucas';
+$idade = 16;
+
+if($idade < 18){
+   echo $nome . ' não é maior de 18 e tem ' . $idade . ' anos de idade.';
+}else{
+   echo $nome . ' é maior de 18 e tem ' . $idade . ' anos de idade.';
+}
+
+echo '<br>';
+echo '___________________________________________________________________________________________________________________ <br>';
+
+
+
+
+
+/**
+ * 1 - Crie um algoritmo que solicite a entrada de um número, e exiba a tabuada de 0 a 10 de acordo com o número solicitado, 
+ *  ex: 
+ *  Entrada = 4
+ *  Saída = 4 X 0 = 0...4 X 10 = 40.
+ * 
+ */
+
+
+
+
+/**
+ * 2 - Solicite a entrada de um número e descubra se um número digitado é par ou ímpar. 
+ *  
+ * 
+ */
+
+
+
+/**
+ *  3 -Faça um algoritmo PHP que receba os valores A e B, imprima-os em ordem crescente em relação aos seus valores. Exemplo, para A=5, B=4. Você deve imprimir na tela: "4 5". 
+ *  
+ * 
+ */
+
+
+/**
+ *  4 - Faça um algoritmo em PHP onde verifica se o valor da variável A é maior ou menor que o valor da variável B. A mensagem a ser impressa deve ser “A maior que B” ou “A menor que B”. 
+ *  
+ * 
+ */
+
+ 
+
+/**
+ *  5 -  Crie um algoritmo para calcular a média final de um aluno, para isso, solicite a entrada de três notas e as insira em um array, por fim, calcule a média geral. Caso a média seja maior ou igual a seis, exiba aprovado, caso contrário, exiba reprovado. Exiba também a média final calculada.
+ * Ex: N1 = 5 | N2 = 10 | N3 = 4 | MG = 6,33 [Aprovado]
+ *  
+ * 
+ */
+
+ 
+/**
+ *  6 -Crie um algoritmo que pergunte ao usuário seu nome e sua idade. Em seguida verifique se a idade é maior ou menor que 18, exiba da seguinte forma: Fulano é maior de 18 e tem XX Anos ou Fulano não é maior de 18 e tem XX Anos.
+ *  
+ * 
+ */
+
+
+
+/**
+ *  7 -Ler um número inteiro entre 1 e 12 e escrever o mês correspondente. Caso o número seja fora desse intervalo, informar que não existe mês com este número.
+ *  
+ * 
+ */
+
+
+ 
+/**
+ *  8 - Crie um algoritmo que solicite um número, e faça o cálculo fatorial do mesmo, exiba o resultado na tela. Ex:
+ * Entrada = 3
+ * Processamento: (3 * 2) * 1
+ * Saída: 6
+ *  
+ * 
+ */
+
+
+
+/**
+ *  9 - Entrar com nome, sexo e idade de uma pessoa. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nome e a mensagem: ACEITA. Caso contrário, imprimir nome e a mensagem: NÃO ACEITA.
+ *  
+ * 
+ */
+
+
+
+/**
+ *  10 - Entrar com 3 números e imprimi-los em ordem decrescente (suponha números diferentes)
+ *  
+ * 
+ */
+
+
+
+/**
+ *  11 -  Ler um número inteiro entre 1 e 12 e escrever o mês correspondente. Caso o número seja fora desse intervalo, informar que não existe   mês com este número
+ *  
+ */
+
+
+/**
+ *  12 - Criar um algoritmos que leia um número e imprima todos os número de 1 até ele .
+ *  
+ * 
+ */
+
+
+
+/**
+ *  13 - Entrar com o número de vezes que se deseja imprimir a palavra SOL e imprimir
+ *  
+ * 
+ */
+
+
+/**
+ *  14 -  Entrar com 20 números e imprimir a somados positivos e o total de números negativos
+ 
+ */
+
+/**
+ *  15 - Criar um algoritmos que imprima a tabuada de um número 
+ */
+
+
+/**
+ *  16 - Criar um algoritmos que entre com uma palavra e imprima conforme o exemplo
+ * ◦ Palavra: sonho
+ * ◦ SONHO
+ * ◦ SONHO SONHO
+ * ◦ SONHO SONHO SONHO
+ * ◦ SONHO SONHO SONHO SONHO
+ */
+
+
+/**
+ *  17 -Ler vários números e informar quantos números entre 100 e 200 foram digitados. Se o valor 0 for lido encerrar a execução
+*/
+
+
+/**
+ *  18 - Chico tem 1,50m e cresce 2 centímetros por ano, enquanto Juca tem 1,10m e cresce 3 centímetros por ano. Construir um algoritmos que calcule e imprima quantos anos serão necessários para que Juca seja maior que Chico
+ */
+
+
+/**
+ *  19 - Armazenar 15 números inteiros em um vetor(array) e imprimir uma mensagem contendo o número e uma das mensagens: par ou ímpar
+ */
+
+
+/**
+ *  20 - Criar um vetor(array) com 20 elementos inteiros. Imprimir o maior e o menor, sem ordenar, o percentual de números pare e média dos elementos do vetor
+ */
+
+
+
+/**
+ *  21 - Criar 2 vetores(array) com 10 números inteiros cada. Gerar e imprimir um vetor dos números não comuns aos e vetores
+ */
---
+++ exercicios.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Tranformar a string em array
+ */
+
+$nome = 'Daniel, Lucas, Guilherme'; //String
+
+
+
+
+/**
+ * Percorrer o array e printar suas posições
+ */
+
+$frutas = array(
+    'Maças',
+    'Pera',
+    'Melancia',
+    'uva'
+); //array
+
